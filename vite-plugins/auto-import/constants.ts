8b9e91bfc15e31c0891aabe383c17247cfb0fb64
bryce <1907004005@gmail.com>
2022-07-28 22:04:39 +0800

    feat: add hot update support

---
+++ vite-plugins/auto-import/constants.ts
@@ -0,0 +1,3 @@
+export const TagReg = /\<van-(\w+)/gmi
+export const BaseComponents = ['icon', 'loading', 'info', 'common', 'wxs', 'mixins']
+
---
+++ vite-plugins/auto-import/types.ts
@@ -0,0 +1,4 @@
+export interface ICtx {
+  srcLibDir: string
+  componentMap: Record<string, Set<string>>
+}
