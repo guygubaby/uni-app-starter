0b082bececa07fc3f1c170d9357192432643a1b8
guygubaby <guygubaby@gmail.com>
2022-09-21 13:55:15 +0800

    chore: lint files

--- src/utils/auth/token.ts
+++ src/utils/auth/token.ts
@@ -1,20 +1,13 @@
-/*
- * @Date: 2022-05-20 15:15:36
- * @LastEditors: YuanBo
- * @Author: YuanBo
- * @LastEditTime: 2022-05-20 15:18:31
- * @FilePath: /my-vue3-project/src/utils/auth/token.ts
- */
+const TOKENKEY = 'app-note-token'
 
-const TOKENKEY = 'app-note-token';
-export function getToken(){
+export function getToken() {
   return uni.getStorageSync(TOKENKEY)
 }
 
-export function setToken(token:string){
+export function setToken(token: string) {
   uni.setStorageSync(TOKENKEY, token)
 }
 
-export function removeToken(){
+export function removeToken() {
   uni.removeStorageSync(TOKENKEY)
 }
--- src/utils/http/index.ts
+++ src/utils/http/index.ts
@@ -1,31 +1,25 @@
-/*
- * @Date: 2022/2/22 10:36 上午
- * @Author: lane
- * @FilePath: /my-vue3-project/src/utils/http/index.ts
- */
-
-import config from "../../config";
-import qs from "qs";
-import {
+import qs from 'qs'
+import config from '../../config'
+import { getToken } from '../auth/token'
+import type {
   getOptions,
   postOptions,
   requestOptions,
   serverResponse,
-} from "./interface";
-import { storageKeys, httpCode } from "./enum";
-import { getToken } from "../auth/token";
+} from './interface'
+import { httpCode, storageKeys } from './enum'
 
-const alertConfirmBtnColor = "#019fba";
+const alertConfirmBtnColor = '#019fba'
 /**
  * 请求前拦截器
  * @param options
  */
 const httpBefore = function (options: requestOptions) {
-  !options.header ? (options.header = {}) : "";
-  let token = getToken() || "";
-  token ? (options.header.Authorization = config.tokenPrefix + token) : "";
-  return options;
-};
+  !options.header ? (options.header = {}) : ''
+  const token = getToken() || ''
+  token ? (options.header.Authorization = config.tokenPrefix + token) : ''
+  return options
+}
 /**
  * 请求后拦截器
  * @param response
@@ -33,29 +27,31 @@ const httpBefore = function (options: requestOptions) {
 const httpAfter = async function (response: any): Promise<serverResponse> {
   // 判断请求状态码
   if (response.statusCode !== 200) {
-    console.warn("请求异常");
+    console.warn('请求异常')
     uni.showModal({
-      title: "服务异常",
+      title: '服务异常',
       content: `错误代码:${response.statusCode}`,
       showCancel: false,
       confirmColor: alertConfirmBtnColor,
-    });
-    uni.hideLoading();
+    })
+    uni.hideLoading()
     return Promise.reject(
-      `服务异常|错误代码:${response.statusCode}|错误信息:http请求异常`
-    );
-  } else {
+      `服务异常|错误代码:${response.statusCode}|错误信息:http请求异常`,
+    )
+  }
+  else {
     // 判断业务状态码
     try {
-      await checkStatusCode(response.data);
+      await checkStatusCode(response.data)
       // console.log('判断业务状态码成功', response.data);
-      return response.data;
-    } catch (err) {
-      console.log(err);
-      throw err;
+      return response.data
+    }
+    catch (err) {
+      console.log(err)
+      throw err
     }
   }
-};
+}
 /**
  * 检测业务码状态
  * @param res
@@ -63,91 +59,91 @@ const httpAfter = async function (response: any): Promise<serverResponse> {
 const checkStatusCode = async function (res: serverResponse) {
   if (res.code === httpCode.FAIL) {
     uni.showModal({
-      title: "系统提示",
+      title: '系统提示',
       content: `${res.msg}`,
       showCancel: false,
       confirmColor: alertConfirmBtnColor,
-    });
-    uni.hideLoading();
-    throw `服务异常|错误代码:${res.code}|错误信息:${res.msg}`;
+    })
+    uni.hideLoading()
+    throw `服务异常|错误代码:${res.code}|错误信息:${res.msg}`
   }
   if (res.code === httpCode.NO_AUTH) {
-    uni.removeStorageSync(storageKeys.OPENID);
-    uni.removeStorageSync(storageKeys.PERSON_NAME);
-    uni.removeStorageSync(storageKeys.TOKEN);
+    uni.removeStorageSync(storageKeys.OPENID)
+    uni.removeStorageSync(storageKeys.PERSON_NAME)
+    uni.removeStorageSync(storageKeys.TOKEN)
 
     uni.showModal({
       title: httpCode.NO_AUTH.toString(),
-      content: `登录过期,点击确定重新登录`,
+      content: '登录过期,点击确定重新登录',
       showCancel: false,
       confirmColor: alertConfirmBtnColor,
       success() {
-        uni.clearStorageSync();
+        uni.clearStorageSync()
         uni.reLaunch({
-          url: "/pkg-main/pages/index/index",
-        });
+          url: '/pkg-main/pages/index/index',
+        })
       },
-    });
-    uni.hideLoading();
-    throw `服务异常|错误代码:${res.code}|错误信息:${res.msg}`;
+    })
+    uni.hideLoading()
+    throw `服务异常|错误代码:${res.code}|错误信息:${res.msg}`
   }
 
   if (res.code !== httpCode.SUCCESS) {
     uni.showModal({
-      title: "服务异常",
+      title: '服务异常',
       content: `错误代码:${res.code}`,
       showCancel: false,
       confirmColor: alertConfirmBtnColor,
-    });
-    uni.hideLoading();
-    throw `服务异常|错误代码:${res.code}|错误信息:${res.msg}`;
+    })
+    uni.hideLoading()
+    throw `服务异常|错误代码:${res.code}|错误信息:${res.msg}`
   }
-};
+}
 
 const http = {
-  get(url: string = "", options: getOptions = {}): Promise<serverResponse> {
-    options = httpBefore(<requestOptions>options);
+  get(url = '', options: getOptions = {}): Promise<serverResponse> {
+    options = httpBefore(<requestOptions>options)
     return new Promise((resolve, reject) => {
       uni.request({
         url: `${config.baseURL}${url || options.url}`,
-        method: "GET",
+        method: 'GET',
         data: {
           ...(options.params || {}),
         },
         header: {
           ...options.header,
         },
-        success: async function (res) {
+        async success(res) {
           try {
-            const rs = await httpAfter(res);
-            resolve(rs);
-          } catch (err) {
-            reject(err);
+            const rs = await httpAfter(res)
+            resolve(rs)
+          }
+          catch (err) {
+            reject(err)
           }
         },
         fail(err) {
-          reject(err);
+          reject(err)
         },
-      });
-    });
+      })
+    })
   },
   post(
-    url: string = "",
+    url = '',
     data: object = {},
-    options: postOptions = {}
+    options: postOptions = {},
   ): Promise<serverResponse> {
-    options = httpBefore(<requestOptions>options);
+    options = httpBefore(<requestOptions>options)
 
-    let queryString = qs.stringify(options.params || {});
-    let URL = `${config.baseURL}${url || options.url}`;
-    if (queryString) {
-      URL = `${config.baseURL}${url || options.url}?${queryString}`;
-    }
+    const queryString = qs.stringify(options.params || {})
+    let URL = `${config.baseURL}${url || options.url}`
+    if (queryString)
+      URL = `${config.baseURL}${url || options.url}?${queryString}`
 
     return new Promise((resolve, reject) => {
       uni.request({
         url: URL,
-        method: "POST",
+        method: 'POST',
         data: {
           ...options.params,
           ...data,
@@ -156,27 +152,28 @@ const http = {
         header: {
           ...options.header,
         },
-        success: async function (res) {
+        async success(res) {
           try {
-            const rs = await httpAfter(res);
-            resolve(rs);
-          } catch (err) {
-            reject(err);
+            const rs = await httpAfter(res)
+            resolve(rs)
+          }
+          catch (err) {
+            reject(err)
           }
         },
         fail(err) {
-          reject(err);
+          reject(err)
         },
-      });
-    });
+      })
+    })
   },
   request(options: requestOptions): Promise<serverResponse> {
-    options = httpBefore(options);
-    let queryString: string = qs.stringify(options.params);
-    let URL = `${config.baseURL}${options.url || options.url}`;
-    if (queryString) {
-      URL = `${config.baseURL}${options.url || options.url}?${queryString}`;
-    }
+    options = httpBefore(options)
+    const queryString: string = qs.stringify(options.params)
+    let URL = `${config.baseURL}${options.url || options.url}`
+    if (queryString)
+      URL = `${config.baseURL}${options.url || options.url}?${queryString}`
+
     return new Promise((resolve, reject) => {
       uni.request({
         url: URL,
@@ -188,20 +185,21 @@ const http = {
         header: {
           ...options.header,
         },
-        success: async function (res) {
+        async success(res) {
           try {
-            const rs = await httpAfter(res);
-            resolve(rs);
-          } catch (err) {
-            reject(err);
+            const rs = await httpAfter(res)
+            resolve(rs)
+          }
+          catch (err) {
+            reject(err)
           }
         },
         fail(err) {
-          reject(err);
+          reject(err)
         },
-      });
-    });
+      })
+    })
   },
-};
+}
 
-export { http };
+export { http }
